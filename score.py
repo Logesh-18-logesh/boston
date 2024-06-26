bf46ccf8150be5256ec617918574c1b9ca4f7465
LOGESH SD <148134953+Logesh-18-logesh@users.noreply.github.com>
2024-06-26 11:31:53 +0530

    Update score.py

--- score.py
+++ score.py
@@ -20,17 +20,22 @@ def init():
 def home():
     return render_template('home.html')
 
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        data = request.get_json()['data']
-        data = np.array(list(data.values())).reshape(1, -1)
-        data = scalar.transform(data)
-        prediction = model.predict(data)
-        return jsonify({"prediction": prediction.tolist()})
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The House price prediction is {}".format(output))
 
-if __name__ == '__main__':
-    init()
-    app.run(host='0.0.0.0', port=31311)
+if __name__ == "__main__":
+    app.run(debug=True)
