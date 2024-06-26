9b3913f191a4a54a39f9dbecf99de02097b9396b
LOGESH SD <148134953+Logesh-18-logesh@users.noreply.github.com>
2024-06-26 11:01:58 +0530

    Update score.py

--- score.py
+++ score.py
@@ -1,7 +1,10 @@
 import json
 import pickle
 import numpy as np
+from flask import Flask, request, jsonify, render_template
 from azureml.core.model import Model
+
+app = Flask(__name__)
 
 def init():
     global model
@@ -13,9 +16,21 @@ def init():
     model = pickle.load(open(model_path, 'rb'))
     scalar = pickle.load(open(scalar_path, 'rb'))
 
-def run(raw_data):
-    data = json.loads(raw_data)['data']
-    data = np.array(list(data.values())).reshape(1, -1)
-    data = scalar.transform(data)
-    prediction = model.predict(data)
-    return json.dumps({"prediction": prediction.tolist()})
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()['data']
+        data = np.array(list(data.values())).reshape(1, -1)
+        data = scalar.transform(data)
+        prediction = model.predict(data)
+        return jsonify({"prediction": prediction.tolist()})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    init()
+    app.run(host='0.0.0.0', port=31311)
