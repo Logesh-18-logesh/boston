61bc409eada09fa3143e51596adb0da7628152ce
LOGESH SD <148134953+Logesh-18-logesh@users.noreply.github.com>
2024-06-26 11:48:52 +0530

    Update score.py

--- score.py
+++ score.py
@@ -1,18 +1,24 @@
 import json
 import pickle
+import os
 import numpy as np
 from azureml.core.model import Model
 
 def init():
-    global model
+    global regmodel
     global scalar
-	@@ -13,9 +16,21 @@ def init():
-    model = pickle.load(open(model_path, 'rb'))
+
+    model_path = Model.get_model_path('regmodel')
+    scalar_path = Model.get_model_path('scaling')
+    regmodel = pickle.load(open(model_path, 'rb'))
     scalar = pickle.load(open(scalar_path, 'rb'))
 
 def run(raw_data):
-    data = json.loads(raw_data)['data']
-    data = np.array(list(data.values())).reshape(1, -1)
-    data = scalar.transform(data)
-    prediction = model.predict(data)
-    return json.dumps({"prediction": prediction.tolist()})
+    try:
+        data = json.loads(raw_data)['data']
+        data = np.array(list(data.values())).reshape(1, -1)
+        data = scalar.transform(data)
+        prediction = regmodel.predict(data)
+        return json.dumps({"prediction": prediction.tolist()})
+    except Exception as e:
+        return json.dumps({"error": str(e)})
